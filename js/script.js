c91e52953890321f4ed961f49524cd61bd2440c0
REMAKI <remakikamel@hotmail.fr>
2017-03-30 14:20:50 +0200

    ajout du carrousel et corrections

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 
   $('button').click(function () {
     $('.error').stop().fadeIn(400).delay(3000).fadeOut(400); //fade out after 3 seconds
+    $('.carousel').carousel()
 });
 
 });
